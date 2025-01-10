1c16b786875c8f38b884162ff158a5866031c80b
rguylan <guylan.richaud@ynov.com>
2025-01-10 09:29:25 +0100

    push1

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-func main() {
-	print("uwu")
+import (
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done bool
 }
 
-//salut je test ma branch guylan
+type TodoPageData struct {
+	PageTitle string
+	Todos []Todo
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
+
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
+}
+
+func main () {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+// git add . git commit -m "msg" git push
